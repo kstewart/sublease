579c32165d0e046f0d1cc9dd5e1ddedce43e153d
Kevin Stewart <kevin@kstewart.me>
2019-09-11 11:33:58 -0700

    Basic skeleton in place

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func getRandomWord(dictionary string) string {
+	word := fmt.Sprintf("{%s}", dictionary)
+	return word
+}
+
+func generateRandomTrailingNumber(size int) int {
+	rand.Seed(time.Now().UnixNano())
+	if size > 0 {
+		return rand.Intn(size)
+	} else {
+		// Default
+		return rand.Intn(4)
+	}
+}
 
 func main() {
-	fmt.Println("Subdomain generator")
+	adjective := getRandomWord("adjective")
+	noun := getRandomWord("noun")
+	trailing := generateRandomTrailingNumber(4)
+	fmt.Printf("%s-%s-%04d\n", adjective, noun, trailing)
 }
